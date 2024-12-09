dfdc37dbf0180bd5f0cf84a239ed3ff82f101ff1
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2024-12-09 11:46:33 -0500

    Corrected spelling errors, updated serviceAreas pk id's to all be unique.

--- scripts/serviceArea.js
+++ scripts/serviceArea.js
@@ -1,4 +1,4 @@
-import { getserviceAreas } from "./database.js"
+import { getServiceAreas } from "./database.js"
 
 //turn getServiceAreas into an array
 //turn getServices into an array
@@ -7,5 +7,9 @@ import { getserviceAreas } from "./database.js"
 //create a function that finds the areas matched based on the serviceArea object
 //must return the array of areas with the specific service
 
-//export
+//export array of arrays to be accessed by event listner.
+
+
+const serviceAreas = getServiceAreas()
+const 
 
